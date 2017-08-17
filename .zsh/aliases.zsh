de2c1fb3bb84dcd940e800e88eafdacba08c3f63
Nils Weber <me@nilsweb.de>
2017-08-17 13:38:08 +0200

    Added alias configuration

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,7 @@
+# Manage dotfiles repository with a simple command
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# Folder shortcuts
+alias dv='cd ~/Developer'
+alias dl='cd ~/Downloads'
+alias dt='cd ~/Desktop'
